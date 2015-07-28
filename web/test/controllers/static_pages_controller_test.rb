9116c4660cca9a773c22c80ff0a294552a6b5c6e
thebeardisred <redbeard@duadikos.com>
2015-07-28 06:32:09 +0000

    looks like crap, but its better

--- web/test/controllers/static_pages_controller_test.rb
+++ web/test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "decide.vote"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | decide.vote"
+    #assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | decide.vote"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | decide.vote"
+    assert_select "title", "About | #{@base_title}"
   end
 end
